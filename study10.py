2bfff3f7c78db94ca4fffeaf1eb3fc7e336a2d8d
Zehra Aykan <120047575+sawat122@users.noreply.github.com>
2022-12-14 18:20:49 +0300

    Update study10.py

--- study10.py
+++ study10.py
@@ -2,8 +2,6 @@
 Bir tane "Bilgisayar" sınıfı oluşturarak bu sınıfa metodlar ve özellikler 
 ekleyin ve bu class'ı kullanmaya çalışın.
 
-Bu sınıfı yazarken öğrendiğimiz özel metodların hepsini tanımlamaya çalışın.
-
 """
 import time
 
